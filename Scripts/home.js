d76e04d1929dd97bc822eaec73912c6667667ef9
AhmedEhab2022 <am8122004@gmail.com>
2024-03-05 19:08:48 +0200

    fix the deployment problem

--- Scripts/home.js
+++ Scripts/home.js
@@ -124,7 +124,7 @@ function createRecipeCard(recipe) {
   recipeCard.innerHTML = `
     <img class="recipe-img" src="${recipe.image}" alt="${recipe.title}" />
     <h4 class="recipe-name">${recipe.title}</h4>
-    <a href="/recipe.html">
+    <a href="/MyRecipe/recipe.html>
     <button type="submit" class="view" id="${recipe.id}">View</button>
     </a>
     `;
